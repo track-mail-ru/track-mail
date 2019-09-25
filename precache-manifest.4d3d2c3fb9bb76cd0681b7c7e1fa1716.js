50532bc646e1ebabf3726a1bfdc7822144a1b673
Alexey Opalev <a.opalev@corp.mail.ru>
2019-09-25 11:03:41 +0300

    Updates

---
+++ precache-manifest.4d3d2c3fb9bb76cd0681b7c7e1fa1716.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "62e138c0a7f414ba282c9a54fefd2aa9",
+    "url": "track-mail-ru.github.io/track-mail/index.html"
+  },
+  {
+    "revision": "f448518655e476d378dc",
+    "url": "track-mail-ru.github.io/track-mail/static/css/main.b4eae362.chunk.css"
+  },
+  {
+    "revision": "72dfb1185c65b2fc0eb2",
+    "url": "track-mail-ru.github.io/track-mail/static/js/2.cbc690c9.chunk.js"
+  },
+  {
+    "revision": "f448518655e476d378dc",
+    "url": "track-mail-ru.github.io/track-mail/static/js/main.50418c58.chunk.js"
+  },
+  {
+    "revision": "021d9fe89c1e30484e09",
+    "url": "track-mail-ru.github.io/track-mail/static/js/runtime-main.69db3457.js"
+  },
+  {
+    "revision": "795fb555bea801821f51202dc10aaa78",
+    "url": "track-mail-ru.github.io/track-mail/static/media/logo.795fb555.svg"
+  }
+]);
