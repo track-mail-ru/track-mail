747d2a738b68e2945618ccb69d752b4f319f9b0f
Alexey Opalev <a.opalev@corp.mail.ru>
2019-09-25 10:36:02 +0300

    Fixed tests; fixed travis

---
+++ convertBytesToHuman.js
@@ -0,0 +1,11 @@
+/*
+ * Функция `convertBytesToHuman` должна принимать
+ * аргумент `bytes` только числового типа.
+ * Необходимо предусмотреть защиту от
+ * передачи аргументов неправильного типа
+ * и класса (например, отрицательные числа)
+ */
+
+export default function convertBytesToHuman(bytes) {
+  // your solution goes here
+}
---
+++ convertBytesToHuman.test.js
@@ -0,0 +1,22 @@
+/*
+ * Необходимо покрыть все возможные
+ * и невозможные кейсы. Например,
+ * convertBytesToHuman(-1) === false,
+ * convertBytesToHuman(-1) !== 1,
+ * convertBytesToHuman('string') === false
+ * convertBytesToHuman(5) === 5
+ */
+
+import convertBytesToHuman from './convertBytesToHuman';
+
+test('Возвращает false для неправильного типа данных', () => {
+  expect(convertBytesToHuman(/* ... */)).toBe(/* ... */)
+  // ...
+});
+
+test('Возвращает корректное значение для чисел', () => {
+  expect(convertBytesToHuman(/* ... */)).toBe(/* ... */)
+  // ...
+});
+
+// другая группа проверок
